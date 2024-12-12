9e5171a7d3766323707b2f9e7b60ec0e0e5a05c2
Roberto <ruzdrei@MacBook-Pro-de-Roberto.local>
2024-12-11 20:30:07 -0600

    CartContext

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import ItemCount from './ItemCount'
+import { Link } from 'react-router-dom'
+import { CartContext } from '../context/CartContext'
 
 const ItemDetail = ({producto}) => {
+  const [compra, setCompra] = useState(false)
+  const {addToCart, cart } = useContext (CartContext)
     const onAdd = (cantidad) => {
-        alert(`Compraste ${cantidad} productos`)
+      setCompra (true)
+      // let cartItem = {
+      //   name:producto.name,
+      //   img:producto.imagen,
+      //   price: producto.price,
+      //   stock:producto.stock,
+      //   id:producto.id
+      //  }
+      //  addToCart (cartItem,cantidad)
+        addToCart (producto,cantidad)
     }
-
+console.log (cart)
 
   return (
    
@@ -15,8 +28,8 @@ const ItemDetail = ({producto}) => {
       <img src={producto.imagen} alt={producto.nombre} />
       <p>{producto.lugar}</p>
       <p>stock:{producto.stock}</p>
-      <p>${producto.price}</p>
-      <ItemCount stock={producto.stock} initial={1} onAdd={onAdd}/>
+      <p>${producto.price}</p> 
+      {compra ? <Link to='/cart'>Ir al Carrito</Link> : <ItemCount stock={producto.stock} initial={1} onAdd={onAdd}/>}
     </div>
   )
 }
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,15 +1,33 @@
 import { createContext, useState } from "react";
 
-
 export const CartContext = createContext ()
 
 export const CartProvider = ({children}) => {
     const [ cart, setCart] = useState ([])
 
+    const addToCart = (item, quantity) => {
+        if(isInCart (item.id)){
+       setCart(
+        cart.map((prod)=>{
+            if(prod.id === item.id){
+                return {...prod, cantidad:prod.cantidad + quantity}
+            }  else {
+                return prod;
+            }
+        })
+       )
+        }else{
+            //sumar un item nuevo al carito
+            setCart ([...cart, {...item, cantidad : quantity }])
+        }
+    }
 
-
+    const isInCart = (id)=>{
+        return cart.some ((prod)=> prod.id === id)
+    }
+    console.log(cart, 'carrito')
 return (
-   <CartContext.Provider value={{cart}}>
+   <CartContext.Provider value={{cart, addToCart}}>
         {children}
 
 
